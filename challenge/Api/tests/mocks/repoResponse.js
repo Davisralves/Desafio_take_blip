c9b2ef0330a6dfb8f56fe18bbcb8972cb77554a5
Davisralves <davialves324@gmail.com>
2022-07-23 15:45:28 -0300

    test: criando mock

---
+++ challenge/Api/tests/mocks/repoResponse.js
@@ -0,0 +1,14 @@
+const response = [
+	{
+		owner: { avatar_url: "avatar1" },
+		full_name: "full_name1",
+		subtitle: "descriptio2",
+	},
+	{
+		owner: { avatar_url: "avatar2" },
+		full_name: "full_name2",
+		subtitle: "description2",
+	},
+];
+
+module.exports = response;
