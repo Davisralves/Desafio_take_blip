d42d86704942be5d98aae595396b328b56696655
Davisralves <davialves324@gmail.com>
2022-07-26 15:20:49 -0300

    fix: corrigindo link da requisição a api e adicionando variavel PORT para caso o arquivo .env não exista a api continue a funcionar

--- challenge/Api/index.js
+++ challenge/Api/index.js
@@ -2,13 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const Controller = require("./controller/getBlipRepos");
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
 app.get("/", Controller.getBlipRepos);
 
-app.listen(process.env.PORT, () => {
-	console.log(`Escutando na porta ${process.env.PORT}`);
+app.listen(PORT, () => {
+	console.log(`Escutando na porta ${PORT}`);
 });
 
-module.exports = app
+module.exports = app;
